96261b87769dd4246afe57a62c1f87ec41dce332
clara <10088152+claraj@users.noreply.github.com>
2021-03-30 18:24:49 -0500

    note on navigation by name

--- client/src/components/PageFooter.vue
+++ client/src/components/PageFooter.vue
@@ -3,11 +3,24 @@
   <div class="navigation-links">
     
     <span class="navigation">
+      
+      <!-- Router-links can use absolute paths, as strings -->
       <router-link to="/">Home</router-link>
+
+      <!-- Or can use the name of the route, defined in the router  -->
+      <!-- This is more flexible because you can change the text of the URL in router/index.js -->
+      <!-- without needing to change the path in all the router-links that use it -->
+      <!-- <router-link v-bind:to="{ name: 'StateList' }">Home</router-link> -->
+
+
     </span>
 
     <span class="navigation">
       <router-link to="/about">About this site</router-link>
+
+      <!-- alternative, navigate by name -->
+      <!-- <router-link v-bind:to="{ name: 'AboutSite' }">About this site</router-link> -->
+
     </span>
 
   </div>
